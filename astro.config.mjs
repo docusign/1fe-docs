7df7451d522aaaf378154cc9d1440ab93a64efcb
abe.hu <abe.hu@docusign.com>
2025-05-15 13:27:16 -0700

    add FAQ section

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,10 +14,7 @@ const isLocalDevelopment = process.env.NODE_ENV === 'development';
 // https://astro.build/config
 export default defineConfig({
   server: {
-		port: DEV_PORT,
-		headers: {
-			"Content-Security-Policy": "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
-		}
+		port: DEV_PORT
 	},
 	output: "static",
 	base: '/',
