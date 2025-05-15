08d1aec4bed491c39ecc7722704227e860bbcb90
abe.hu <abe.hu@docusign.com>
2025-05-15 10:32:15 -0700

    add csp

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,9 +15,6 @@ const isLocalDevelopment = process.env.NODE_ENV === 'development';
 export default defineConfig({
   server: {
 		port: DEV_PORT,
-		headers: {
-			"Content-Security-Policy": "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
-		}
 	},
 	output: "static",
 	base: '/',
