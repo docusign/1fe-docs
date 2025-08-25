392e2d9867e9d42d0a1655d1df55f98947ac27f5
Mohit Khatri <mohit.khatri@docusign.com>
2025-08-25 11:23:11 -0700

    Removing changes so the PR isn't that complicated.

--- config-utils/sidebar.ts
+++ config-utils/sidebar.ts
@@ -3,25 +3,36 @@ export const sidebarConfig = [
     label: "Start Here",
     autogenerate: { directory: "start-here" },
     collapsed: true,
+    // attrs: { style: "text-transform: 'capitalized'" },
   },
   {
     label: "Tutorials",
     autogenerate: { directory: "tutorials" },
     collapsed: true,
+    // attrs: { style: "text-transform: 'capitalized'" },
   },
   {
     label: "Learning",
     autogenerate: { directory: "learning" },
     collapsed: true,
+    // attrs: { style: "text-transform: 'capitalized'" },
   },
   {
     label: "How-to Guides",
     autogenerate: { directory: "how-to-guides" },
     collapsed: true,
+    // attrs: { style: "text-transform: 'capitalized'" },
   },
   {
     label: "Reference",
     autogenerate: { directory: "reference" },
     collapsed: true,
+    // attrs: { style: "text-transform: 'capitalized'" },
   },
+  // {
+  //   label: 'Explanation',
+  //   autogenerate: { directory: 'explanation' },
+  //   collapsed: true,
+  //   // attrs: { style: "text-transform: 'capitalized'" },
+  // }
 ];
